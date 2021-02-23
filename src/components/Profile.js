ace52ab2358763c29c089d1be52c4ca0ee4d7456
Martijn van der Blom <martijn@teamofteams.nl>
2021-02-23 21:47:33 +0100

    WIP calling a private oauth-ed api

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth0} from "@auth0/auth0-react";
 import JSONPretty from "react-json-pretty";
 
 const Profile = () => {
-    const  { user, isAuthenticated } = useAuth0();
+    const  { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const [ userData, setUserData ] = useState(null)
+
+
+    const getUserData = async () => {
+
+            const accessToken = await getAccessTokenSilently({
+                audience: `tentplanner-api`,
+                scope: "read:messages",
+            });
+            console.log('accessToken', accessToken)
+            const url = `/api/private`;
+            const response = await fetch(url, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            console.log('pre-backend-call')
+            const { data } = await response.json();
+            console.log('Friet', data)
+            setUserData(data);
+    }
+
 
     return (
         isAuthenticated &&
@@ -13,8 +35,12 @@ const Profile = () => {
             <p>{user.email}</p>
             <hr />
             <JSONPretty data={user} />
+            <hr />
+            <button onClick={() => getUserData()}>Get Userdata</button>
+            {userData && <JSONPretty data={userData} /> }
         </div>
     )
 }
+
 
 export default Profile
